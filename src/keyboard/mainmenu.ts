155583f8276e48730981bd7a11da38f99d61e047
kw1z1 <sashachekhonina@mail.ru>
2025-11-14 20:38:41 +1000

    menu+ bot

--- src/keyboard/mainmenu.ts
+++ src/keyboard/mainmenu.ts
@@ -1,23 +1,35 @@
 import { Keyboard } from '@maxhub/max-bot-api';
 
 const keyboard_start = Keyboard.inlineKeyboard([
-
     [
+        Keyboard.button.callback('🤖 GigaChat', 'gigachat'),
         Keyboard.button.callback('Помощь❓', 'help')
     ],
 ]);
 
 const keyboard_mainmenu = Keyboard.inlineKeyboard([
-
     [
+        Keyboard.button.callback('🤖 GigaChat', 'gigachat'),
         Keyboard.button.callback('Помощь❓', 'help')
     ],
 ]);
 
 const keyboard_helpmenu = Keyboard.inlineKeyboard([
-
     [
         Keyboard.button.callback('Контакты', 'contact'),
-        Keyboard.button.callback('🔙Назад'), 'backtomenu'),
+        Keyboard.button.callback('🔙Назад', 'backtomenu'),
     ],
 ]);
+
+const keyboard_gigachat = Keyboard.inlineKeyboard([
+    [
+        Keyboard.button.callback('🔙 В главное меню', 'backtomenu')
+    ],
+]);
+
+export { 
+    keyboard_start, 
+    keyboard_mainmenu, 
+    keyboard_helpmenu, 
+    keyboard_gigachat 
+};
